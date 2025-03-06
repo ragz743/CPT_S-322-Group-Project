d6834af254e33cb7f5a77c2a25a6f679cabd58d6
Seth-Trimberger <106629997+Seth-Trimberger@users.noreply.github.com>
2025-03-06 12:55:24 -0800

    Added the teacher SideBar

---
+++ Teacher/JavaScriptFiles/TeStudent.js
@@ -0,0 +1,66 @@
+
+
+
+
+const AnnoucmentDiv = document.getElementById('Annoucments');
+
+
+AnnoucmentDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/TeAnncucments.html'
+
+})
+
+const AssignmentDiv = document.getElementById('Assignments');
+
+
+AssignmentDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/TeAssignments.html'
+
+})
+
+const DiscussionDiv = document.getElementById('Discussions');
+
+
+DiscussionDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/TeDiscussions.html'
+
+})
+
+const FilesDiv = document.getElementById('Files');
+
+
+FilesDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/TeFiles.html'
+
+})
+
+const GradesDiv = document.getElementById('Grades');
+
+
+GradesDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/TeGrades.html'
+
+})
+
+const MessagesDiv = document.getElementById('Messages');
+
+MessagesDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/TeMessages.html'
+
+})
+
+const SyllbusDiv = document.getElementById('Syllbus');
+
+
+SyllbusDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/TeSyllbus.html'
+
+})
+
+const modulesDiv = document.getElementById('Modules');
+
+
+modulesDiv.addEventListener('click',function(){
+    window.location.href = 'SideBarHtml/Temodules.html'
+
+})
